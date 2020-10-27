4da59fd80033acbf1c8f6c0c474d53e64650c5b2
Cipo <ezequiel.baruf@gmail.com>
2020-10-27 12:38:00 -0300

    se agregó tailwind y postcss, se agrego firebase

--- front/services/firebase.js
+++ front/services/firebase.js
@@ -1,2 +1,12 @@
-import {FIREBASE_CONFIG} from '../helpers/config';
+import firebase from 'firebase';
+import { FIREBASE_CONFIG } from '../helpers/config';
 
+try {
+  firebase.initializeApp(FIREBASE_CONFIG);
+  console.log('Inicializado');
+} catch (err) {
+  console.log(err);
+}
+
+export const db = firebase.database();
+export const auth = firebase.auth();
---
+++ front/services/prueba.js
@@ -0,0 +1,5 @@
+import { db } from './firebase';
+
+const all = () => {
+    db.
+};
