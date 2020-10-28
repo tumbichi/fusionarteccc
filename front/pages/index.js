5c3416a5ce41e54862dd3ddb518e875b17fcf195
Cipo <ezequiel.baruf@gmail.com>
2020-10-28 11:13:38 -0300

    configuracion styled components

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,9 +1,14 @@
 /* eslint-disable react/react-in-jsx-scope */
+import styled from 'styled-components';
+
+const StyleBox = styled.div.attrs({
+  className: 'flex justify-center items-center h-screen bg-yellow-300',
+})``;
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <StyleBox>
       <div className="bg-red-600 text-white">AGUANTE NEW Y RIVER LOCOOOOO✨</div>
-    </div>
+    </StyleBox>
   );
 }
