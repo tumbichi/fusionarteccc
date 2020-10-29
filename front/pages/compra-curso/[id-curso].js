9447354592330af5e1c3cdfdfb428d71609fa4dd
Guillermo <guilletejera@hotmail.com>
2020-10-29 16:01:18 -0300

    Se corrigio error

--- front/pages/compra-curso/[id-curso].js
+++ front/pages/compra-curso/[id-curso].js
@@ -1,4 +1,11 @@
+import { useRouter } from 'next/router';
+
 const CompraCursoPage = () => {
+  const router = useRouter();
+
+  const goToMercadoPago = (course) => {
+    router.push(`/`); //url mercado pago
+  };
   return <>COMPRAR CURSO</>;
 };
 export default CompraCursoPage;
--- front/pages/index.js
+++ front/pages/index.js
@@ -1,11 +1,18 @@
 /* eslint-disable react/react-in-jsx-scope */
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
 
 const StyleBox = styled.div.attrs({
   className: 'flex justify-center items-center h-screen bg-yellow-300',
 })``;
 
 const HomePage = () => {
+  const router = useRouter();
+
+  const goToCourse = (course) => {
+    router.push(`/cursos/${course}`);
+  };
+
   return (
     <StyleBox>
       <div className="bg-red-600 text-white">AGUANTE NEW Y RIVER LOCOOOOO✨</div>
