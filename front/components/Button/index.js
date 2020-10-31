d6613908b45703e66617ddf005b0992a7a3dab58
JuanpiiFogliaco <fogliacp@gmail.com>
2020-10-31 16:01:21 -0300

    agregue propTypes y defaultProps en Button

--- front/components/Button/index.js
+++ front/components/Button/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {Wrapper} from './styles';
+import { Wrapper } from './styles';
+import PropTypes from 'prop-types';
 
-const Button = ({text , onClick}) => {
-    return (
-        <Wrapper onClick={onClick}> 
-            {text}
-        </Wrapper>
-    )
-}
+const Button = ({ text, onClick }) => {
+  return <Wrapper onClick={onClick}>{text}</Wrapper>;
+};
 
-export default Button
+Button.defaultProps = {
+  text: 'click',
+  onClick: () => {},
+};
+
+Button.propTypes = {
+    text: PropTypes.string,
+    onClick: PropTypes.func
+};
+
+export default Button;
