075cbc44e34337762b0abbe73b1bdc048423a9a8
tumbichi <germanviglietti@gmail.com>
2020-11-07 16:17:37 -0300

    Cambios en atributos de Button

--- front/components/Button/index.js
+++ front/components/Button/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { Wrapper } from './styles';
 import PropTypes from 'prop-types';
 
-const Button = ({ text, onClick }) => {
+const Button = ({ height, width, text, color, onClick }) => {
+
+  
   return <Wrapper onClick={onClick}>{text}</Wrapper>;
 };
 
 Button.defaultProps = {
   text: 'click',
+  color: 'primary'
   onClick: () => {},
 };
 
 Button.propTypes = {
   text: PropTypes.string,
+  color: PropTypes.oneOf(['primary', 'secondary', 'accent'])
   onClick: PropTypes.func,
 };
 
