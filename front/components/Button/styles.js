2e3f8e9caa2fdc334ccce3e22e8d67d3d91696b8
Cipo <ezequiel.baruf@gmail.com>
2020-11-15 16:10:46 -0300

    Se ultimaron detalles en el component button

--- front/components/Button/styles.js
+++ front/components/Button/styles.js
@@ -1,8 +1,36 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/button-has-type */
 import styled from 'styled-components';
 
-export const Wrapper = styled.button`
-  background: whitesmoke;
-  max-height: 60px;
-  padding: 7px;
-  cursor: pointer;
-`;
+export const IconContainer = styled.div.attrs({
+  className: 'mx-1',
+})``;
+
+export const Wrapper = ({ children, color, size }) => {
+  let bg;
+  let bgHover;
+  let text;
+  let textHover;
+  if (color === 'primary') {
+    bg = 'primary';
+    bgHover = 'secondary';
+    text = 'secondary';
+    textHover = 'primary';
+  }
+  if (color === 'secondary') {
+    bg = 'primary';
+    bgHover = 'acent';
+    text = 'secondary';
+    textHover = 'secondary';
+  }
+  return (
+    <button
+      className={`flex items-center justify-center px-2 py-2
+       focus:shadow-2xl focus:outline-none 
+       hover:bg-${bgHover} bg-${bg} hover:text-${textHover} text-${text} 
+       rounded-full  md:tracking-wide font-semibold `}
+    >
+      {children}
+    </button>
+  );
+};
