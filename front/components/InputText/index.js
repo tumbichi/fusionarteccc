283057ac37932931be3960ea1d040a7de33539fb
CipoBaruf <ezequiel.baruf@gmail.com>
2020-11-15 22:06:00 -0300

    modificacion de estilos del inputText

--- front/components/InputText/index.js
+++ front/components/InputText/index.js
@@ -1,11 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import PropTypes, { string } from 'prop-types';
+import { InputContainer, Input, Label } from './styles';
 
 const InputText = ({ name, value, placeholder, iconLeft, iconRight, errorText, type, onChange, onFocus }) => {
   // eslint-disable-next-line no-unused-vars
   return (
-    <input name={name} type={type} value={value} placeholder={placeholder} onChange={onChange} onFocus={onFocus} />
+    <InputContainer>
+      <Input name={name} type={type} value={value} placeholder={placeholder} onChange={onChange} onFocus={onFocus} />
+      {errorText && <Label>{errorText}</Label>}
+    </InputContainer>
   );
 };
 
@@ -31,7 +35,6 @@ InputText.propTypes = {
   type: PropTypes.string,
   onChange: PropTypes.func,
   onFocus: PropTypes.func,
-
 };
 
 export default InputText;
