7ebfd1864153d56d698c8616dfacbafd401fa1e5
CipoBaruf <ezequiel.baruf@gmail.com>
2020-11-22 16:07:59 -0300

    se volvio al logo hecho por new

--- front/components/Toolbar/index.js
+++ front/components/Toolbar/index.js
@@ -5,7 +5,7 @@ import { ToolbarWrapper, ImageWrapper as Logo } from './styles';
 const Toolbar = ({ right, goToHome }) => {
   return (
     <ToolbarWrapper>
-      <Logo src="../../static/images/logo.png" onClick={goToHome}/>
+      <Logo src="../../static/images/logo.png" onClick={goToHome} />
       {right}
     </ToolbarWrapper>
   );
