ae9c744ab7502b54e6380ce1b61c8920302f42e7
JuanpiiFogliaco <fogliacp@gmail.com>
2020-11-30 22:51:40 -0300

    model user-table

---
+++ front/models/user.js
@@ -0,0 +1,9 @@
+export class UserTable {
+  constructor(id, nombre, apellido,email, fecha_creacion){
+    this.id= id,
+    this.nombre = nombre,
+    this.apellido = apellido,
+    this.email = email, 
+    this.fecha_creacion = fecha_creacion
+  }
+}
