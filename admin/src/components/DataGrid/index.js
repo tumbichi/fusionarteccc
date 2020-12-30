7e6a41016712564fd3bca9cd4eea941fb5fabcaf
JuanpiiFogliaco <fogliacp@gmail.com>
2020-12-29 22:14:01 -0300

    data grid en home con service get all users

--- admin/src/components/DataGrid/index.js
+++ admin/src/components/DataGrid/index.js
@@ -1,34 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDataGrid from '@inovua/reactdatagrid-community';
 import '@inovua/reactdatagrid-community/index.css';
+import { db } from '../../services/firebase';
 
 const DataGrid = () => {
-  const columns = [
-    { name: 'name', header: 'Name', minWidth: 50, defaultFlex: 2 },
-    { name: 'age', header: 'Age', maxWidth: 1000, defaultFlex: 1 },
-  ];
-
   const gridStyle = { minHeight: 550 };
-
-  const dataSource = [
-    { id: 1, name: 'John McQueen', age: 35 },
-    { id: 2, name: 'Mary Stones', age: 25 },
-    { id: 3, name: 'Robert Fil', age: 27 },
-    { id: 4, name: 'Roger Robson', age: 81 },
-    { id: 5, name: 'Billary Konwik', age: 18 },
-    { id: 6, name: 'Bob Martin', age: 18 },
-    { id: 7, name: 'Matthew Richardson', age: 54 },
-    { id: 8, name: 'Ritchie Peterson', age: 54 },
-    { id: 9, name: 'Bryan Martin', age: 40 },
-    { id: 10, name: 'Mark Martin', age: 44 },
-    { id: 11, name: 'Michelle Sebastian', age: 24 },
-    { id: 12, name: 'Michelle Sullivan', age: 61 },
-    { id: 13, name: 'Jordan Bike', age: 16 },
-    { id: 14, name: 'Nelson Ford', age: 34 },
-    { id: 15, name: 'Tim Cheap', age: 3 },
-    { id: 16, name: 'Robert Carlson', age: 31 },
-    { id: 17, name: 'Johny Perterson', age: 40 },
+  const columns = [
+    { name: 'nombre', header: 'Nombre', maxWidth: 400, defaultFlex: 1 },
+    { name: 'apellido', header: 'Apellido', maxWidth: 400, defaultFlex: 1 },
+    { name: 'email', header: 'Email', maxWidth: 400, defaultFlex: 1 },
+    {
+      name: 'fechaNacimiento',
+      header: 'Fecha de Nacimiento',
+      maxWidth: 400,
+      defaultFlex: 1,
+    },
+    { name: '_key', header: 'key', maxWidth: 400, defaultFlex: 1 },
   ];
+  const [dataSource, setDataSource] = useState([]);
+
+  useEffect(() => {
+    const items = [];
+    db.ref('Usuarios').once('value', (snap) => {
+      snap.forEach((child) => {
+        items.push({
+          _key: child.key,
+          ...child.val(),
+        });
+      });
+      setDataSource(items);
+    });
+  }, []);
 
   return (
     <ReactDataGrid
@@ -39,5 +41,4 @@ const DataGrid = () => {
     />
   );
 };
-
 export default DataGrid;
--- admin/src/pages/home.js
+++ admin/src/pages/home.js
@@ -6,11 +6,20 @@ import { auth } from '../services';
 const Home = () => {
   // eslint-disable-next-line no-unused-vars
   const [user, loading, error] = useAuthState(auth);
-  useEffect(() => {
-    if (!user && !loading) {
-      window.location = 'http://localhost:3000/login';
+
+  const Redirect = () => {
+    /*  if (!user && !loading) {
+      window.location = 'http://localhost:3001/login';
+    } else */
+    if (user) {
+      window.location = 'http://localhost:3001/';
     }
+  };
+
+  useEffect(() => {
+    Redirect();
   }, [user, loading]);
+
   return <DataGrid />;
 };
 
