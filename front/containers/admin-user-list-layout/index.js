5590b057005a7014f8ba6b6315907c1195aec770
CipoBaruf <ezequiel.baruf@gmail.com>
2021-01-03 15:21:46 -0300

    modify correct indentation

--- front/containers/admin-user-list-layout/index.js
+++ front/containers/admin-user-list-layout/index.js
@@ -3,30 +3,30 @@ import { useSelector, useDispatch } from 'react-redux';
 import { UserListWrapper } from './styles';
 import List from '../../components/List';
 import ListItem from '../../components/ListItem';
-import { suscribeToUser } from '../../services'
+import { suscribeToUser } from '../../services';
 import { setUsers } from '../../store/actions/user.action';
 
 const UserListLayout = () => {
-    const dispatch = useDispatch();
-    const users = useSelector((state) => state.user.users);
-    const data = [1, 2, 3, 4, 5];
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.user.users);
+  const data = [1, 2, 3, 4, 5];
 
-    useEffect(() => {
-        suscribeToUser((data) => {
-            dispatch(setUsers(data));
-        })
-    }, []);
+  useEffect(() => {
+    suscribeToUser((data) => {
+      dispatch(setUsers(data));
+    });
+  }, []);
 
-    console.log('users', users)
-    return (
-        <UserListWrapper height={'500px'} width={'500px'}>
-            <List>
-                {users.map((item) => {
-                    return <ListItem data={item}/>
-                })}
-            </List>
-        </UserListWrapper>
-    )
-}
+  console.log('users', users);
+  return (
+    <UserListWrapper height={'500px'} width={'500px'}>
+      <List>
+        {users.map((item) => {
+          return <ListItem data={item} />;
+        })}
+      </List>
+    </UserListWrapper>
+  );
+};
 
-export default UserListLayout
+export default UserListLayout;
