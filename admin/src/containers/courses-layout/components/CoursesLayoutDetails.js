17d6c542e2c58f9c1ace960f08a9939bcefbb005
JuanpiiFogliaco <fogliacp@gmail.com>
2021-01-03 17:12:19 -0300

    components CoursesLayoutDetails CoursesLayoutEdit

--- admin/src/containers/courses-layout/components/CoursesLayoutDetails.js
+++ admin/src/containers/courses-layout/components/CoursesLayoutDetails.js
@@ -1,7 +1,21 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable react/button-has-type */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-const CoursesLayoutDetails = () => {
-  return <div>details</div>;
+const CoursesLayoutDetails = ({ course, onBack }) => {
+  return (
+    <div>
+      <button onClick={onBack}>Volver</button>
+      <h1>Detalle del curso: {course.titulo} </h1>
+      <img src={course.urlImagen} height="auto" width="420px" />
+      <p>
+        Descripcion: <br /> {course.descripcion}
+      </p>
+      <p>Precio: {course.precio}</p>
+    </div>
+  );
 };
 
 export default CoursesLayoutDetails;
