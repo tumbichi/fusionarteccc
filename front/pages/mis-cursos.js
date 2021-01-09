93a8f2c4f74e7267071ef35797e3fb0072099f09
CipoBaruf <ezequiel.baruf@gmail.com>
2021-01-09 17:42:47 -0300

    added toolbar

--- front/pages/mis-cursos.js
+++ front/pages/mis-cursos.js
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router';
 import Head from 'next/head';
 import SeeMoreCoursesContainer from '../containers/see-more-courses-layout';
 import ListPurchasedCoursesContainer from '../containers/list-purchased-courses-layout';
+import ToolbarContainer from '../containers/toolbar-layout';
 /* eslint-disable react/react-in-jsx-scope */
 
 const MisCursosPage = () => {
@@ -19,10 +20,9 @@ const MisCursosPage = () => {
         <title>Mis cursos</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-
-      <SeeMoreCoursesContainer/>
-      <ListPurchasedCoursesContainer/>
-
+      <ToolbarContainer />
+      <SeeMoreCoursesContainer />
+      <ListPurchasedCoursesContainer />
     </>
   );
 };
