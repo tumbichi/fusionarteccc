0b72b0968a3346f4c4af5c8c5ef36a905d472a29
CipoBaruf <ezequiel.baruf@gmail.com>
2021-03-21 18:13:20 -0300

    resolved typo

--- front/components/AboutUs/index.js
+++ front/components/AboutUs/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AboutUs = () => {
   return (
     <div className="p-12 md:px-64 lg:px-1/2 h-96 flex flex-col text-center items-center justify-center">
-      <p className="text-3xl text-white">Sobre Nosotres</p>
+      <p className="text-3xl text-white">Sobre Nosotros</p>
       <hr className="bg-gray-400 h-0.5 w-full bg-opacity-5" />
       <p className="text-xl text-white pt-2">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos illo tenetur doloremque perferendis sint
